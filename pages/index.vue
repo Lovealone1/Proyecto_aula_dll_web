71ef4872f78f4edde4902c0e89eaad7a9f000e7a
Lovealone1 <89592158+Lovealone1@users.noreply.github.com>
2023-10-05 17:50:05 -0500

    Feat: Creación de la página inicial y formulario de inicio de sesión

---
+++ pages/index.vue
@@ -0,0 +1,90 @@
+<template>
+    <v-container>
+      <v-app>
+        <v-app-bar class="header">
+          <v-toolbar-title>
+            <v-img
+              src="https://cdn-icons-png.flaticon.com/512/869/869687.png"
+              alt="Logo"
+              max-height="40"
+            />
+          </v-toolbar-title>
+  
+          <v-spacer></v-spacer>
+  
+          <div class="search-bar-container">
+            <v-card-text>
+                <v-text-field
+                :loading="loading"
+                density="compact"
+                variant="solo"
+                label="Buscar productos"
+                append-inner-icon="mdi-magnify"
+                single-line
+                hide-details
+                @click:append-inner="onClick"
+                color="#D3DBE0"
+                ></v-text-field>
+            </v-card-text>
+          </div>
+  
+          <v-spacer></v-spacer> <!-- Agregamos otro spacer para empujar los botones a la derecha -->
+  
+          <v-btn icon>
+            <v-icon @click="goToCart">mdi-cart</v-icon>
+          </v-btn>
+  
+          <v-btn icon>
+            <v-icon @click="goToUser">mdi-account</v-icon>
+          </v-btn>
+        </v-app-bar>
+  
+        <v-row>
+          <v-col>
+            <!-- Contenido de tu página -->
+          </v-col>
+        </v-row>
+      </v-app>
+    </v-container>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        search: '',
+      };
+    },
+    methods: {
+      goToCart() {
+        console.log('Ir al carrito');
+      },
+      goToUser() {
+      console.log('Ir al perfil de usuario');
+      // Redirecciona al componente de login
+      this.$router.push({ name: 'login' });
+    },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .search-bar-container {
+    flex: 1; /* Aprovecha el espacio disponible */
+    display: flex;
+    justify-content: left;
+    align-items: left;
+  }
+  
+  /* Estilo adicional para asegurar que el ícono en la barra de búsqueda no se mueva hacia la derecha */
+  .search-bar-container .v-input__icon {
+    left: 0;
+  }
+  
+  /* Estilo adicional para ajustar el ícono en la barra de búsqueda */
+  .search-bar-container {
+    margin-left: -450px; /* Ajusta el margen según sea necesario */
+  }
+
+  </style>
+  
---
+++ pages/login.vue
@@ -0,0 +1,98 @@
+<template>
+    <div class="login-container">
+      <v-img
+        class="mx-auto my-6"
+        max-width="228"
+        src="https://www.pngkit.com/png/full/364-3642224_clarity-ecommerce-logo-logo-e-commerce-png.png"
+      ></v-img>
+      
+  
+      <v-card
+        class="mx-auto pa-12 pb-8"
+        elevation="1"
+        max-width="448"
+        rounded="lg"
+      >
+  
+        <div class="text-subtitle-1 text-medium-emphasis">Correo electrónico</div>
+  
+        <v-text-field
+          v-model="email"
+          density="compact"
+          placeholder="Correo electrónico"
+          prepend-inner-icon="mdi-email-outline"
+          variant="underlined"
+        ></v-text-field>
+  
+        <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
+          Contraseña
+  
+          <a
+            class="text-body-2 font-weight-regular text-grey"
+            href="#"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Olvidaste tu contraseña?</a>
+        </div>
+  
+        <v-text-field
+          v-model="password"
+          :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+          :type="visible ? 'text' : 'password'"
+          density="compact"
+          placeholder="Ingresa tu contraseña"
+          prepend-inner-icon="mdi-lock-outline"
+          variant="underlined"
+          @click:append-inner="visible = !visible"
+        ></v-text-field>
+  
+        <v-btn
+          block
+          class="mb-8"
+          color="blue"
+          size="large"
+          variant="outlined"
+          @click="login"
+        >
+          Iniciar sesión
+        </v-btn>
+  
+        <v-card-text class="text-center">
+          <a
+            class="text-grey text-decoration-none"
+            href="./register"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            ¡Registrate ahora! <v-icon icon="mdi-chevron-right"></v-icon>
+          </a>
+        </v-card-text>
+  
+      </v-card>
+    </div>
+  </template>
+  
+  <script>
+  
+  export default {
+
+  };
+  definePageMeta({
+  layout: "blank",
+  });
+  </script>
+  
+  <style scoped>
+  .login-container{
+  
+  justify-content: center;
+  align-items: center;
+  height: 150vh;
+  }
+  .login-container v-card {
+  width: 100%;
+  }
+  
+  
+  </style>
