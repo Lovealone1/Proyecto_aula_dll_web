e9618064adcbfb210a83a72f60eaaedc7e6c9f26
Lovealone1 <89592158+Lovealone1@users.noreply.github.com>
2023-10-14 23:32:44 -0500

    Fix: Dialog edicion de usuario

--- pages/panelProducto.vue
+++ pages/panelProducto.vue
@@ -1,22 +1,6 @@
 <template>
   <div>
-    <crear_producto ref="crear_producto"/>
-    <products-table ref="productsTable" />
-    
+    <panel-producto/>
+    <crud-producto/>
   </div>
 </template>
-
-<script>
-import ProductsTable from '@/components/panel_producto.vue'; // Asegúrate de tener la ruta correcta
-import crear_producto from '~/components/crud_producto.vue';
-
-export default {
-  components: {
-    ProductsTable,
-    crear_producto,
-  },
-  methods: {
-
-  }
-};
-</script>
