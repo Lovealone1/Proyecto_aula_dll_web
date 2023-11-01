9eea988a17fa4dbc3076a4adb43ff52eb0773fd7
Lovealone1 <89592158+Lovealone1@users.noreply.github.com>
2023-10-31 20:09:24 -0500

    Feat: Actualizacion de producto en el backend

--- backend/controllers/productoController.js
+++ backend/controllers/productoController.js
@@ -60,8 +60,122 @@ const obtener_portada_producto = async function(req, res){
     });
 }
 
+const obtener_producto_admin = async function(req, res){
+    if(req.user){
+        let id = req.params['id'];
+
+        try{
+            let producto = await Producto.findById({_id: id});
+
+            res.status(200).send(producto);
+        }catch{
+            res.status(200).send(undefined);
+        }
+    }else{
+        res.status(500).send({data:undefined,message: 'ErrorToken'});
+    }
+}
+
+const actualizar_producto_admin = async function(req,res){
+    if(req.user){
+        let data = req.body; 
+        let id = req.params['id'];
+
+        let productos = await Producto.find({titulo: data.titulo});
+        console.log(productos.length);
+
+        if (productos.length>=1) {
+            if(productos[0]._id == id){
+                if(req.files){
+                    //Registro exitoso
+                    var img_path = req.files.portada.path
+                    var str_img = img_path.split('\\');
+                    var str_portada = str_img[2];
+    
+                    data.portada = str_portada; 
+                    data.slug = slugify(data.titulo);
+                    
+                    try {
+                        let producto = await Producto.findByIdAndUpdate({_id: id},{
+                            titulo: data.titulo, 
+                            categoria: data.categoria,
+                            descripcion: data.descripcion,
+                            estado: data.estado, 
+                            descuento: data.descuento, 
+                            portada: data.portada,
+                        });
+                        res.status(200).send({data:producto});
+                    } catch (error) {
+                        res.status(200).send({data:undefined,message: 'No se pudo guardar el producto'});
+                    }
+                }else{
+                     data.slug = slugify(data.titulo);
+                     
+                     try {
+                         let producto = await Producto.findByIdAndUpdate({_id: id},{
+                             titulo: data.titulo, 
+                             categoria: data.categoria,
+                             descripcion: data.descripcion,
+                             estado: data.estado, 
+                             descuento: data.descuento, 
+                         });
+                         res.status(200).send({data:producto});
+                     } catch (error) {
+                         res.status(200).send({data:undefined,message: 'No se pudo guardar el producto'});
+                     }
+                }
+            }else{
+                res.status(200).send({data:undefined,message: 'El producto ya existe'});
+            }
+        }else{
+            if(req.files){
+                //Registro exitoso
+                var img_path = req.files.portada.path
+                var str_img = img_path.split('\\');
+                var str_portada = str_img[2];
+
+                data.portada = str_portada; 
+                data.slug = slugify(data.titulo);
+                
+                try {
+                    let producto = await Producto.findByIdAndUpdate({_id: id},{
+                        titulo: data.titulo, 
+                        categoria: data.categoria,
+                        descripcion: data.descripcion,
+                        estado: data.estado, 
+                        descuento: data.descuento, 
+                        portada: data.portada,
+                    });
+                    res.status(200).send({data:producto});
+                } catch (error) {
+                    res.status(200).send({data:undefined,message: 'No se pudo guardar el producto'});
+                }
+            }else{
+                 data.slug = slugify(data.titulo);
+                 
+                 try {
+                     let producto = await Producto.findByIdAndUpdate({_id: id},{
+                         titulo: data.titulo, 
+                         categoria: data.categoria,
+                         descripcion: data.descripcion,
+                         estado: data.estado, 
+                         descuento: data.descuento, 
+                     });
+                     res.status(200).send({data:producto});
+                 } catch (error) {
+                     res.status(200).send({data:undefined,message: 'No se pudo guardar el producto'});
+                 }
+            }
+        }
+    }else{
+        res.status(500).send({data:undefined,message: 'ErrorToken'});
+    }
+}
+
 module.exports = {
     registro_producto_admin,
     listar_productos_admin,
-    obtener_portada_producto
+    obtener_portada_producto,
+    obtener_producto_admin,
+    actualizar_producto_admin
 }
--- backend/routes/producto.js
+++ backend/routes/producto.js
@@ -8,5 +8,7 @@ var api = express.Router();
 api.post('/registro_producto_admin',[authenticate.decodeToken,path], productoController.registro_producto_admin);
 api.get('/listar_productos_admin/:filtro?',authenticate.decodeToken, productoController.listar_productos_admin);
 api.get('/obtener_portada_producto/:img',productoController.obtener_portada_producto);
+api.get('/obtener_producto_admin/:id',authenticate.decodeToken,productoController.obtener_producto_admin);
+api.put('/actualizar_producto_admin/:id',[authenticate.decodeToken,path],productoController.actualizar_producto_admin);
 
 module.exports = api
