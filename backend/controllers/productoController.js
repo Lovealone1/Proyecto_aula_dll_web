a93018b4b0ab546e9349d8c4f2b969dcc98503a7
Lovealone1 <89592158+Lovealone1@users.noreply.github.com>
2023-11-13 23:06:45 -0500

    Feat: Metodos para listar los ingresos de productos al inventario

--- backend/controllers/productoController.js
+++ backend/controllers/productoController.js
@@ -496,6 +496,50 @@ const cambiar_estado_producto_admin = async function(req, res){
     }
 }
 
+const obtener_ingresos_admin = async function(req,res){
+    if(req.user){
+        let inicio = req.params['inicio'];
+        let hasta = req.params['hasta'];
+
+        var ingresos = await Ingreso.find({
+            createdAt: {
+                $gte: new Date(inicio+'T00:00:00'),
+                $lt: new Date(hasta+'T00:00:00'),
+            }
+        });
+        res.status(200).send(ingresos);
+
+    }else{
+        res.status(500).send({data:undefined,message: 'ErrorToken'});
+    }
+}
+
+const obtener_comprobante_ingreso = async function(req,res){
+    let name = req.params['name'];
+
+    fs.stat('./uploads/facturas/'+name,function(err){
+        if(err){
+            let path_img = './uploads/default.jpg';
+            res.status(200).sendFile(path.resolve(path_img));
+        }else{
+            let path_img = './uploads/facturas/'+name;
+            res.status(200).sendFile(path.resolve(path_img));
+        }
+    });
+}
+
+const obtener_detalles_ingresos_admin = async function(req,res){
+    if(req.user){
+        let id = req.params['id'];
+
+        var ingreso = await Ingreso.findById({_id: id});
+        var detalles = await Ingreso_detalle.find({ingreso: id}).populate('producto').populate('variedad');
+        res.status(200).send({ingreso,detalles});
+
+    }else{
+        res.status(500).send({data:undefined,message: 'ErrorToken'});
+    }
+}
 
 module.exports = {
     registro_producto_admin,
@@ -516,5 +560,8 @@ module.exports = {
     listar_categorias_admin,
     crear_subcategoria_admin,
     eliminar_subcategoria_admin,
-    cambiar_estado_producto_admin
+    cambiar_estado_producto_admin,
+    obtener_ingresos_admin,
+    obtener_comprobante_ingreso,
+    obtener_detalles_ingresos_admin
 }
--- backend/routes/producto.js
+++ backend/routes/producto.js
@@ -20,6 +20,9 @@ api.get('/obtener_variedades_producto/:id',authenticate.decodeToken, productoCon
 api.delete('/eliminar_variedad_producto/:id',authenticate.decodeToken, productoController.eliminar_variedad_producto);
 
 api.post('/registro_ingreso_admin',[authenticate.decodeToken,path_ingreso], productoController.registro_ingreso_admin);
+api.get('/obtener_ingresos_admin/:inicio/:hasta',authenticate.decodeToken, productoController.obtener_ingresos_admin);
+api.get('/obtener_comprobante_ingreso/:name',  productoController.obtener_comprobante_ingreso);
+api.get('/obtener_detalles_ingresos_admin/:id',authenticate.decodeToken, productoController.obtener_detalles_ingresos_admin);
 
 api.post('/subir_imagen_producto_admin',[authenticate.decodeToken,path_galeria], productoController.subir_imagen_producto_admin);
 api.get('/obtener_galeria_producto/:img',productoController.obtener_galeria_producto);
